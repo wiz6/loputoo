6e3bd329ea533323f81bc9ddc584d3e95bf02235
Mart Madisson <martt6@hot.ee>
2015-04-26 19:18:51 +0300

    empty

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 $config = array(
-    'cdn_uri' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'
+    'cdn_uri' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+    'sss' => 'test'
 );
